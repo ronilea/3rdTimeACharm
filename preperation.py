9ea5b7ee0eae94a82ceb7b29d87d05c187714b35
morV <mor.testa96@gmail.com>
2021-06-03 11:43:34 +0300

    new commit

--- preperation.py
+++ preperation.py
@@ -2,7 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-
+CRIME_PRIMARY = {'BATTERY':0, 'THEFT':1, 'CRIMINAL DAMAGE':2, 'DECEPTIVE '
+                                                              'PRACTICE':3,
+                 'ASSAULT':4}
 def preprocessing(df):
     print(df.shape)
     del df['IUCR']
@@ -12,6 +14,7 @@ def preprocessing(df):
     del df['Case Number']
     del df['Year']
     del df['Updated On']
+    del df['Unnamed: 0']
     df.dropna(inplace=True)
     del df['X Coordinate']
     del df['Y Coordinate']
@@ -19,9 +22,12 @@ def preprocessing(df):
     del df['Longitude']
     del df['Location']
     df = pd.get_dummies(df, columns=['Arrest'])
-    print(df.shape)
-    print('h')
-    df.describe(include='all').to_csv("train_desc_after_filter.csv",index=False)
+    df = pd.get_dummies(df, columns=['Domestic'])
+    df = pd.get_dummies(df, columns=['District'])
+    df['Date'] = pd.to_datetime(df.Date)
+    df.replace(to_replace=CRIME_PRIMARY, inplace=True)
+    print(df.dtypes)
+    # df.describe(include='all').to_csv("train_desc_after.csv",index=False)
 
 
 if __name__ == '__main__':
