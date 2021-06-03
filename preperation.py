c65d3b195f55ad3f19bbc24bfe8f3136c9cab887
morV <mor.testa96@gmail.com>
2021-06-03 12:17:21 +0300

    ni hi

--- preperation.py
+++ preperation.py
@@ -27,7 +27,6 @@ def preprocessing(df):
     df['Date'] = pd.to_datetime(df.Date)
     df.replace(to_replace=CRIME_PRIMARY, inplace=True)
     print(df.dtypes)
-    print("hi ")
     print(df.Date.dt.time)
     # df.describe(include='all').to_csv("train_desc_after.csv",index=False)
 
